c920c4a1add52a1832875dc88a1c1e6ad147fdac
Mayon85 <62755258+Mayon85@users.noreply.github.com>
2020-05-20 15:01:06 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+
+// SELECT ELEMENTS
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const showDate = document.querySelector(".display-date p");
+
+
+// DISPLAY DATE AND TIME
+
+let today = new Date();
+let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+months = months[today.getMonth()];
+let year = today.getFullYear();
+let date = today.getDate();
+
+let current_date =`${date} ${months} ${year}`;
+
+showDate.innerHTML = current_date;
+
+
+// APP DATA
+const weather = {}; 
+
+weather.temperature = {
+    unit : "celcius"
+}
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+
+// CHECK IS BROWSER SUPPORTS GEOLOCALISATION
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTLM = "<p> Votre navigateur ne supporte pas la géolocalisation</p>";
+}
+
+//SET USER'S POSITION
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR IF ISSUE WITH GEOLOCALISATION SERVICE
+function showError(error) {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTLM = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API
+
+const key = "cb32c7b51eb9bb812fbb0e2c4b498c5a";
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data) {
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function() {
+        displayWeather();
+    });
+}
+
+// DISPLAY WEATHER 
+function displayWeather () {
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
